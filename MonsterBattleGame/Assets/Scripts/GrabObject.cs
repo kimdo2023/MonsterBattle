6975b867ebb0c5f4c45f4050e3742dc7068f0f00
Kim Do <khdo@usc.edu>
2022-09-20 20:43:51 -0700

    updated grabobject

--- MonsterBattleGame/Assets/Scripts/GrabObject.cs
+++ MonsterBattleGame/Assets/Scripts/GrabObject.cs
@@ -14,13 +14,13 @@ public class GrabObject : MonoBehaviour
         RaycastHit2D grabCheck = Physics2D.Raycast(grabDetect.position, Vector2.right * transform.localScale, rayDist);
         if(grabCheck.collider != null && grabCheck.collider.tag == "Fire")
         {
-            if (Input.GetKey(KeyCode.E))
+            if (Input.GetKeyDown(KeyCode.E))
             {
                 grabCheck.collider.gameObject.transform.parent = boxHolder;
                 grabCheck.collider.gameObject.transform.position = boxHolder.position;
                 grabCheck.collider.gameObject.GetComponent<Rigidbody2D>().isKinematic = true;
             }
-            else
+            else 
             {
                 grabCheck.collider.gameObject.transform.parent = null;
                 grabCheck.collider.gameObject.GetComponent<Rigidbody2D>().isKinematic = false;
